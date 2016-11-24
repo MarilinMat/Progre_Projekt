b585019ccbd99f7dd18ec6e5977c3e3c0602152c
MarilinMat <marilinmattik@gmail.com>
2016-11-24 11:13:12 +0200

    kood+pildikaust

--- tralallalalalallalalalalalal.py
+++ tralallalalalallalalalalalal.py
@@ -1,1 +1,13 @@
 import pygame
+pygame.init()
+ekraani_suurus = pygame.display.set_mode((900, 600))
+pygame.display.set_caption("Memory")
+taust = pygame.image.load("pildid/tasutaks.jpg")
+#ekraani_pind.fill(taust,(0,0))
+pygame.display.blit(taust, (0,0))
+#pygame.display.flip()
+while True:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        break
+pygame.quit()
