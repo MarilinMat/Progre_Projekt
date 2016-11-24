c0e6194bed5e0571b8642398d99bc05574fd3031
MarilinMat <marilinmattik@gmail.com>
2016-11-24 11:32:40 +0200

    Kood õige ja kustutasin pildi

--- tralallalalalallalalalalalal.py
+++ tralallalalalallalalalalalal.py
@@ -2,10 +2,11 @@ import pygame
 pygame.init()
 ekraani_suurus = pygame.display.set_mode((900, 600))
 pygame.display.set_caption("Memory")
-taust = pygame.image.load("pildid/tasutaks.jpg")
-#ekraani_pind.fill(taust,(0,0))
-pygame.display.blit(taust, (0,0))
-#pygame.display.flip()
+taust = pygame.image.load("pildid/taustaks.jpg")
+ekraani_suurus.fill((0,0,0))
+ekraani_suurus.blit(taust, (0, 0))
+#pygame.display.blit(taust, (0,0))
+pygame.display.flip()
 while True:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
