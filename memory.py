9297ad9e534e83b02340c56003e90a4e04e44132
MarilinMat <marilinmattik@gmail.com>
2016-11-30 16:15:16 +0200

    eemaldasin üleliigsed asjad

--- memory.py
+++ memory.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.init()
 ekraani_suurus = pygame.display.set_mode((900, 600))
 pygame.display.set_caption("Memory")
@@ -6,11 +7,11 @@ taust= pygame.image.load("pildid/taust.jpg")
 
 ekraani_suurus.fill((0,0,0))
 ekraani_suurus.blit(taust, (0, 0))
-#pygame.display.blit(taust, (0,0))
 pygame.display.flip()
+
 while True:
     event = pygame.event.poll()
     if event.type == pygame.QUIT:
         break
+    
 pygame.quit()
-#proov
